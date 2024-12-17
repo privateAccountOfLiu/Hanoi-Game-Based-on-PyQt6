f274e9d28c33b9ca3cb28b52bcd58c4126213989
privateAccountOfLiu <160343662+privateAccountOfLiu@users.noreply.github.com>
2024-12-18 01:16:17 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def load_game_before():
     before_widget.setWindowTitle('Hanoi Game')
     before_widget.setWindowIcon(QtGui.QIcon(hanoi_game_ui.get_dir('./ui/Hanoi.png')))
     game_start_btn: QtWidgets.QPushButton = before_widget.pushButton
-    level_combox = QtWidgets.QComboBox = before_widget.comboBox
+    level_combox: QtWidgets.QComboBox = before_widget.comboBox
     game_start_btn.clicked.connect(lambda: game_start(before_widget, level_combox))
     before_widget.show()
 
